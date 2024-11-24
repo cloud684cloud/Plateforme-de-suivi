df43fa13db209684b58f037dd7d14731c7f301d6
khadija el moqnia <101595820+khadija-elmoqnia@users.noreply.github.com>
2024-11-24 03:16:51 +0000

    json dans funct for build

--- netlify/functions/firebaseInit.js
+++ netlify/functions/firebaseInit.js
@@ -3,13 +3,13 @@ const admin = require("firebase-admin");
 
 
 // Utilisez le chemin absolu vers le fichier serviceAccountKey.json à la racine
-const serviceAccount = require("/workspaces/codespaces-express/serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-// Fonction pour initialiser Firebase si ce n'est pas déjà fait
+// Fonction pour initialiser Firebase i ce n'est pas déjà fait
 const initializeFirebase = () => {
   if (!admin.apps.length) {
     admin.initializeApp({
--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,18 +1,21 @@
 const admin = require("firebase-admin");
-const initializeFirebase = require("./firebaseInit"); // Ensure correct path
+const initializeFirebase = require("./firebaseInit");
 
 exports.handler = async function(event, context) {
-  initializeFirebase();  // Initialize Firebase
+  initializeFirebase();  // Initialize Firebase only once
+  
   try {
     const snapshot = await admin.firestore().collection('test').get();
     const data = snapshot.docs.map(doc => doc.data());
     console.log('Fetched data:', data);  // Log fetched data
+    
     return {
       statusCode: 200,
       body: JSON.stringify(data)
     };
   } catch (error) {
     console.error('Error fetching data:', error);  // Log any errors
+    
     return {
       statusCode: 500,
       body: `Error fetching data: ${error.message}`
